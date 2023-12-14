11a7194e9d3418b5c3416aed2754a319c5d37cb8
Cologero <lennonalvarez530@gmail.com>
2023-12-14 11:59:33 -0800

    save files

--- FInal mini game/Assets/Scripts/Coin.cs
+++ FInal mini game/Assets/Scripts/Coin.cs
@@ -4,12 +4,20 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
+    private AudioSource _coinAudio;
+
+    private void Start()
+    {
+        _coinAudio = GetComponet<AudioSource>();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
             Debug.Log("Player collected the coin.");
             GameObject.Find("Canvas").GetComponent<UIManager>().UpdateCoinCount();
+            _coinAudio.Play(coinSound);
             Destroy(this.gameObject);
         }
     }
